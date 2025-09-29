29623e2018a7e046c416733d9d28d5944997a5c3
Jarod <jarodhila77@gmail.com>
2025-09-29 22:01:34 +0200

    Addeed tests for multipuls of 3 and 5

--- tests/test_fizzbuzz.py
+++ tests/test_fizzbuzz.py
@@ -1,10 +1,21 @@
 import unittest
 from src.fizzbuzz import fizzbuzz
 
-
 class TestFizzBuzz(unittest.TestCase):
     def test_regular_number(self):
         self.assertEqual(fizzbuzz(1), "1")
         self.assertEqual(fizzbuzz(2), "2")
-        if __name__ == '__main__':
-            unittest.main()
+
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "Fizz")
+        self.assertEqual(fizzbuzz(6), "Fizz")
+
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "Buzz")
+        self.assertEqual(fizzbuzz(10), "Buzz")
+
+    def test_fizzbuzz(self):
+        self.assertEqual(fizzbuzz(15), "FizzBuzz")
+        self.assertEqual(fizzbuzz(30), "FizzBuzz")
+if __name__ == '__main__':
+    unittest.main()
